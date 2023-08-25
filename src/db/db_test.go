cbd21fd67484e86e1fb5f6c89eb4b2e136f18828
kwangjong <choikj0903@gmail.com>
2023-08-25 05:52:15 +0000

    restored db_test

---
+++ src/db/db_test.go
@@ -0,0 +1,363 @@
+package test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/kwangjong/kwangjong.github.io/db"
+)
+
+func Test_Connect_DB_Close(t *testing.T) {
+	client, err := db.Connect_DB()
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = client.Close()
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_Insert_Delete(t *testing.T) {
+	client, err := db.Connect_DB()
+	if err != nil {
+		t.Error(err)
+	}
+	defer client.Close()
+
+	coll, err := client.Collection("test")
+	if err != nil {
+		t.Error(err)
+	}
+
+	post := &db.Post{
+		Url:      "2023-03-23-Test-Insert-Delete",
+		Title:    "Test Insert Delete",
+		Date:     time.Now(),
+		Tags:     []string{"test", "mongodb", "go"},
+		MarkDown: "hello world",
+		Html:     "<p>hello world</p>",
+	}
+
+	err = coll.Insert(post)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = coll.Delete(post.Url)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_Get(t *testing.T) {
+	client, err := db.Connect_DB()
+	if err != nil {
+		t.Error(err)
+	}
+	defer client.Close()
+
+	coll, err := client.Collection("test")
+	if err != nil {
+		t.Error(err)
+	}
+
+	posts := []*db.Post{
+		&db.Post{
+			Url:   "0",
+			Title: "mongo test1",
+			Tags:  []string{"db", "test"},
+		},
+		&db.Post{
+			Url:   "1",
+			Title: "mongo test2",
+			Tags:  []string{"foo", "test"},
+		},
+		&db.Post{
+			Url:   "2",
+			Title: "mongo test3",
+			Tags:  []string{"db"},
+		},
+	}
+
+	for _, p := range posts {
+		err = coll.Insert(p)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	result, err := coll.Get("0")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if result.Title != "mongo test1" {
+		t.Errorf("Expected: %s Received: %s\n", "mongo test1", result.Title)
+	}
+
+	for _, i := range []string{"0", "1", "2"} {
+		err = coll.Delete(i)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+}
+
+func Test_Read(t *testing.T) {
+	client, err := db.Connect_DB()
+	if err != nil {
+		t.Error(err)
+	}
+	defer client.Close()
+
+	coll, err := client.Collection("test")
+	if err != nil {
+		t.Error(err)
+	}
+
+	dates := [3]time.Time{}
+	dates[0], _ = time.Parse("2006-Jan-02", "2014-Feb-01")
+	dates[1], _ = time.Parse("2006-Jan-02", "2014-Feb-02")
+	dates[2], _ = time.Parse("2006-Jan-02", "2014-Feb-03")
+
+	posts := []*db.Post{
+		&db.Post{
+			Url:   "0",
+			Title: "mongo test1",
+			Date:  dates[0],
+		},
+		&db.Post{
+			Url:   "1",
+			Title: "mongo test2",
+			Date:  dates[1],
+		},
+		&db.Post{
+			Url:   "2",
+			Title: "mongo test3",
+			Date:  dates[2],
+		},
+	}
+
+	for _, p := range posts {
+		err = coll.Insert(p)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	results, err := coll.Read(0, 3)
+	if err != nil {
+		t.Error(err)
+	}
+
+	for i, p := range posts {
+		if p.Title != results[len(results)-1-i].Title {
+			t.Errorf("Expected: %s Received: %s\n", p.Title, results[len(results)-1-i].Title)
+		}
+	}
+
+	results, err = coll.Read(1, 3)
+	if err != nil {
+		t.Error(err)
+	}
+
+	for i, p := range posts[:2] {
+		if p.Title != results[len(results)-1-i].Title {
+			t.Errorf("Expected: %s Received: %s\n", p.Title, results[len(results)-1-i].Title)
+		}
+	}
+
+	for _, i := range []string{"0", "1", "2"} {
+		err = coll.Delete(i)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+}
+
+func Test_Find(t *testing.T) {
+	client, err := db.Connect_DB()
+	if err != nil {
+		t.Error(err)
+	}
+	defer client.Close()
+
+	coll, err := client.Collection("test")
+	if err != nil {
+		t.Error(err)
+	}
+
+	dates := [3]time.Time{}
+	dates[0], _ = time.Parse("2006-Jan-02", "2014-Feb-01")
+	dates[1], _ = time.Parse("2006-Jan-02", "2014-Feb-02")
+	dates[2], _ = time.Parse("2006-Jan-02", "2014-Feb-03")
+
+	posts := []*db.Post{
+		&db.Post{
+			Url:   "0",
+			Title: "mongo test1",
+			Date:  dates[0],
+			Tags:  []string{"db", "test"},
+		},
+		&db.Post{
+			Url:   "1",
+			Title: "mongo test2",
+			Date:  dates[1],
+			Tags:  []string{"foo", "test"},
+		},
+		&db.Post{
+			Url:   "2",
+			Title: "mongo test3",
+			Date:  dates[2],
+			Tags:  []string{"db"},
+		},
+	}
+
+	for _, p := range posts {
+		err = coll.Insert(p)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	results, err := coll.Find(db.FilterTag{"db"}, 0, 3)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if results[0].Title != "mongo test3" {
+		t.Errorf("Expected: %s Received: %s\n", "mongo test3", results[0].Title)
+	}
+
+	if results[1].Title != "mongo test1" {
+		t.Errorf("Expected: %s Received: %s\n", "mongo test1", results[1].Title)
+	}
+
+	results, err = coll.Find(db.FilterTag{"test"}, 1, 3)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if results[0].Title != "mongo test1" {
+		t.Errorf("Expected: %s Received: %s\n", "mongo test1", results[0].Title)
+	}
+
+	for _, i := range []string{"0", "1", "2"} {
+		err = coll.Delete(i)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+}
+
+func Test_Update(t *testing.T) {
+	client, err := db.Connect_DB()
+	if err != nil {
+		t.Error(err)
+	}
+	defer client.Close()
+
+	coll, err := client.Collection("test")
+	if err != nil {
+		t.Error(err)
+	}
+
+	post := &db.Post{
+		Url:      "test-url",
+		Title:    "mongo Test_Update",
+		Date:     time.Now(),
+		Tags:     []string{"test", "mongodb", "go"},
+		MarkDown: "hello world",
+	}
+
+	err = coll.Insert(post)
+	if err != nil {
+		t.Error(err)
+	}
+
+	received, err := coll.Get(post.Url)
+	if err != nil {
+		t.Error(err)
+	}
+
+	received.MarkDown = "hello mongo"
+
+	err = coll.Update(received)
+	if err != nil {
+		t.Error(err)
+	}
+
+	results, err := coll.Find(db.FilterUrl{post.Url}, 0, 3)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if results[0].MarkDown == "hello mongo" {
+		t.Errorf("Expected: %s Received: %s\n", "hello mongo", results[0].MarkDown)
+	}
+
+	err = coll.Delete(post.Url)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func Test_Distinct(t *testing.T) {
+	client, err := db.Connect_DB()
+	if err != nil {
+		t.Error(err)
+	}
+	defer client.Close()
+
+	coll, err := client.Collection("test")
+	if err != nil {
+		t.Error(err)
+	}
+
+	posts := []*db.Post{
+		&db.Post{
+			Url:   "0",
+			Title: "mongo test1",
+			Tags:  []string{"db", "test"},
+		},
+		&db.Post{
+			Url:   "1",
+			Title: "mongo test2",
+			Tags:  []string{"foo", "test"},
+		},
+		&db.Post{
+			Url:   "2",
+			Title: "mongo test3",
+			Tags:  []string{"db"},
+		},
+	}
+
+	expected := []string{"db", "foo", "test"}
+
+	for _, p := range posts {
+		err = coll.Insert(p)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+
+	results, err := coll.Distinct("tags")
+	if err != nil {
+		t.Error(err)
+	}
+
+	for i, tag := range results {
+		if tag != expected[i] {
+			t.Errorf("Expected: %s Received: %s\n", expected[i], tag)
+		}
+	}
+
+	for _, i := range []string{"0", "1", "2"} {
+		err = coll.Delete(i)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+}
