edb29fb19a819cb9486bb96bef327c19cfcb4092
KJ Choi <choikj0903@gmail.com>
2025-03-09 04:14:40 +0900

    Update server.go

--- src/server/server.go
+++ src/server/server.go
@@ -22,7 +22,7 @@ func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Token")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Api-Key, Token")
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusNoContent)
